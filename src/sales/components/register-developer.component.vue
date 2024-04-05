877fcfc7c394f9e96f526992449e8b35a4ea7a6e
Angel Velasquez <pcisavel@upc.edu.pe>
2024-04-05 17:06:45 -0500

    feat(register-developer): added register developer component.

---
+++ src/sales/components/register-developer.component.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+  name: "register-developer",
+  data() {
+    return {
+      firstName: '',
+      lastName: ''
+    }
+  },
+  methods: {
+    onSubmit() {
+      console.log('Registering developer');
+      this.$emit('developer-registered', {
+        firstName: this.firstName,
+        lastName: this.lastName
+      });
+    }
+  }
+}
+</script>
+
+
+<template>
+  <div>
+    <h2>New Developer</h2>
+    <div>
+      <form @submit.prevent="onSubmit">
+        <div>
+          <label for="firstName">First Name: </label>
+          <input type="text" id="firstName" v-model="firstName" required/>
+        </div>
+        <div>
+          <label for="lastName">Last Name: </label>
+          <input type="text" id="lastName" v-model="lastName" required/>
+        </div>
+        <div>
+          <button type="submit">Register</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
